10bcdd2d7297c193cd1499ab4bafc9fc6152eae3
NicolasMarques1610 <nicolasmarques1205@gmail.com>
2023-08-15 23:33:29 -0300

    realizado modificações no código

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,28 +1,44 @@
-const validaMetodoPagamento = ['credito', 'debito', 'dinheiro'];
 const cardapio = [
-    { codigo: 'cafe', descricao: 'Café', valor: 3.00},
-    { codigo: 'chantily', descricao: 'Chantily', valor: 1.50, requere: 'cafe'},
-    { codigo: 'suco', descricao: 'Suco Natural', valor: 6.20},
-    { codigo: 'sanduiche', descricao: 'Sanduíche', valor: 6.50},
-    { codigo: 'queijo', descricao: 'Queijo', valor: 2.00, requere: 'sanduiche'},
-    { codigo: 'salgado', descricao: 'Salgado', valor: 7.25},
-    { codigo: 'combo1', descricao: '1 Suco e 1 Sanduíche', valor: 9.50},
-    { codigo: 'combo2', descricao: '1 Café e 1 Sanduíche', valor: 7.50},
+    { codigo: 'cafe', valor: 3.00},
+    { codigo: 'chantily', valor: 1.50, requere: 'cafe'},
+    { codigo: 'suco', valor: 6.20},
+    { codigo: 'sanduiche', valor: 6.50},
+    { codigo: 'queijo', valor: 2.00, requere: 'sanduiche'},
+    { codigo: 'salgado', valor: 7.25},
+    { codigo: 'combo1', valor: 9.50},
+    { codigo: 'combo2', valor: 7.50},
 ]
 
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-        if(!validaMetodoPagamento.includes(metodoDePagamento)) return "Forma de pagamento inválida!";
-        if(!itens.length) return "Não há itens no carrinho de compra!";
+        if(!this.verificaMetodoPagamento(metodoDePagamento)) return "Forma de pagamento inválida!";
+        if(!this.verificaCarrinhoVazio(itens)) return "Não há itens no carrinho de compra!";
 
-        itens = itens.map(i => { return { codigo: i.split(',')[0], quantidade: i.split(',')[1]}});
+        itens = this.mapeamentoItens(itens);
 
         if(!this.verificaItem(itens)) return "Item inválido!";
         if(!this.verificaQuantidade(itens)) return "Quantidade inválida!";
         if(!this.verificaItemExtra(itens)) return "Item extra não pode ser pedido sem o principal";
 
-        return "R$ " + this.pagamentoTotal(itens, metodoDePagamento).toFixed(2).replace(".", ",");
+        return `R$ ${this.pagamentoTotal(itens, metodoDePagamento).toFixed(2).replace(".", ",")}`;
+    }
+
+    verificaMetodoPagamento(metodoDePagamento) {
+        return ['credito', 'debito', 'dinheiro'].includes(metodoDePagamento);
+    }
+
+    verificaCarrinhoVazio(itens) {
+        if(!itens.length)
+            return false;
+        
+        return true;
+    }
+    
+    mapeamentoItens(itens) {
+        return itens = itens.map(i => { 
+            return { codigo: i.split(',')[0], quantidade: i.split(',')[1]}
+        });
     }
 
     verificaItem(itens) {
@@ -46,6 +62,7 @@ class CaixaDaLanchonete {
     }
 
     verificaItemExtra(itens) {
+
         if(itens.map(it => it.codigo).includes('chantily') && !itens.map(it => it.codigo).includes('cafe'))
             return false;
         
@@ -67,21 +84,19 @@ class CaixaDaLanchonete {
             }
         }
 
-        if(metodoDePagamento == validaMetodoPagamento[0]) {
-            return this.pagamentoCredito(valorTotal);
-        } else if(metodoDePagamento == validaMetodoPagamento[2]) {
-            return this.pagamentoDinheiro(valorTotal);
-        } else return valorTotal;
+        return valorTotal * this.taxaPagamento(metodoDePagamento);
     }
 
-    pagamentoDinheiro(valorTotal) {
-        return valorTotal * 0.95;
+    taxaPagamento(metodoDePagamento) {
+        switch (metodoDePagamento) {
+            case 'credito':
+                return 1.03;
+            case 'dinheiro':
+                return 0.95;
+            default:
+                return 1;
+        }
     }
-
-    pagamentoCredito(valorTotal) {
-        return valorTotal * 1.03;
-    }
-
 }
 
 export { CaixaDaLanchonete };
